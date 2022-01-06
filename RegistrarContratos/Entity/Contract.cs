08d6badc97e3b13dc2a71549d89f7ded4c68dda1
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-06 09:19:14 -0300

    Criacao da classe Installment

--- RegistrarContratos/Entity/Contract.cs
+++ RegistrarContratos/Entity/Contract.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RegistrarContratos.Entity
 {
@@ -7,6 +8,7 @@ namespace RegistrarContratos.Entity
         public int Number { get; set; }
         public DateTime Date { get; set; }
         public double TotalValue { get; set; }
+        public List<Installment> Installments { get; set; } = new List<Installment>();
 
         public Contract(int number, DateTime date, double totalValue)
         {
---
+++ RegistrarContratos/Entity/Installment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RegistrarContratos.Entity
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+    }
+}
