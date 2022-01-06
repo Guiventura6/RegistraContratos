d9aa0ee4d219db5c124ed58d6833fc464e59587c
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-06 09:14:08 -0300

    Criacao da classe Contract

---
+++ RegistrarContratos/Entity/Contract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RegistrarContratos.Entity
+{
+    class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+    }
+}
--- RegistrarContratos/Program.cs
+++ RegistrarContratos/Program.cs
@@ -6,7 +6,7 @@ namespace RegistrarContratos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
