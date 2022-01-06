3dd12a6b7dbb5d3da36a1b5c8a762778af7b2296
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-06 09:07:28 -0300

    Projeto iniciado

---
+++ RegistrarContratos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegistrarContratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
